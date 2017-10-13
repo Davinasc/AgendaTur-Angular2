b0d4fbbf247fae31b1961426a2379178798458e3
Davi Nascimento <davi-nasc@hotmail.com>
2017-10-13 14:27:42 -0300

    Limpando projeto

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,89 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Dashboard',
+    icon: 'nb-home',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'MENU',
+    group: true,
+  },
+  {
+    title: 'Vendedores',
+    icon: 'ion-ios-people-outline',
+    link: '/pages/ui-features',
+    children: [
+      {
+        title: 'Adicionar novo',
+        link: '#',
+      },
+      {
+        title: 'Ver todos',
+        link: '#',
+      },
+    ],
+  },
+  {
+    title: 'Guias',
+    icon: 'ion-ios-flag-outline',
+    children: [
+      {
+        title: 'Adicionar novo',
+        link: '#',
+      },
+      {
+        title: 'Ver todos',
+        link: '#',
+      },
+    ],
+  },
+  {
+    title: 'Passeios',
+    icon: 'ion-android-bus',
+    children: [
+      {
+        title: 'Adicionar novo',
+        link: '#',
+      },
+      {
+        title: 'Ver todos',
+        link: '#',
+      },
+    ],
+  },
+  {
+    title: 'Rotas',
+    icon: 'nb-location',
+    children: [
+      {
+        title: 'Adicionar novo',
+        link: '#',
+      },
+      {
+        title: 'Ver todos',
+        link: '#',
+      },
+    ],
+  },
+  {
+    title: 'Analytics',
+    icon: 'nb-bar-chart',
+    children: [
+      {
+        title: 'Echarts',
+        link: '/pages/charts/echarts',
+      },
+      {
+        title: 'Charts.js',
+        link: '/pages/charts/chartjs',
+      },
+      {
+        title: 'D3',
+        link: '/pages/charts/d3',
+      },
+    ],
+  },
+];
